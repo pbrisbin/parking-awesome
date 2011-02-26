2d1b8e62b35d88f072f44f005d1f0fc30d0b0b28
Ken Mazaika <kmazaika@gmail.com>
2011-02-26 10:18:20 -0500

    railsify app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParkingAwesome::Application.config.secret_token = '5fb642c3a6a455782f2fdb32f58ec4752d9adbdcf7e6776b6288f0e8544386c42a9a1bda9a0c9afb48221d3541613c8a7ce1ec54c11be95d72b7e7cfae22c253'
