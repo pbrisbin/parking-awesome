9302b0beb01f33343c1a04f96f8a0ce5251af85a
patrick brisbin <pbrisbin@gmail.com>
2011-02-26 11:15:11 -0500

    refactor, generalize civic api code

--- public/js/SnowRoute.js
+++ public/js/SnowRoute.js
@@ -1,34 +1,13 @@
-/* take a longitute and latitue and make a 'bbox' argument for the json 
- * request which boxes that point
+/* look for snow route features at the given point; if features, call 
+ * success(features), else call failure.
  */
-function mkBbox(_long, _lat) {
-  // todo: zero-volume box ok?
-  var longMin = _long;
-  var longMax = _long;
-  var latMin  = _lat;
-  var latMax  = _lat;
-
-  return longMax + ',' + latMin + ',' + longMin + ',' + latMax;
-}
-
-/* make the dataset request to see if the given long/lat is on a snow 
- * route or not; if there are features, call the callback with them as 
- * argument
- */
-function isSnowRoute(_long, _lat, _callback) {
-  var dataset = 'bos_snow_routes';
-  var bbox    = mkBbox(_long, _lat);
-
-  $.ajax({
-      url: "http://civicapi.com/" + dataset,
-      dataType: 'jsonp',
-      data: {
-          "bbox": bbox
-      },
-      success: function(data) {
-        if (data.features.length) {
-          _callback(data.features);
-        }
-      }
+function isSnowRoute(lng, lat, success, failure) {
+  civicAPI('bos_snow_routes', lng, lat, function(data) {
+    if (data.features.length) {
+      success(data.features);
+    }
+    else {
+      failure;
+    }
   });
 }
