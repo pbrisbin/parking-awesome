c5488b59e045b1bbc5832a5be0726939daf08a47
Ken Mazaika <kmazaika@gmail.com>
2011-02-26 15:17:40 -0500

    add some normilization methods.

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,4 +1,5 @@
 class Parking
+  DIRECTIONS = {'N' => 0, 'NE' => 45, 'E' => 90, 'SE' => 135, 'S' => 180, 'SW' => 225, 'W' => 270, 'NW' => 315}
   attr_accessor :address, :heading
   
   def initialize(attributes={})
@@ -13,6 +14,28 @@ class Parking
       "right" => right }
   end
   
+  
+  def self.normalize_distance(distance_string)
+    feet = distance_string.match(/(.*?) feet/)
+    return feet[1].to_i if feet
+    
+    miles = distance_string.match(/(.*?) mile/)
+    return miles[1].to_f * 5280 if miles
+    
+    nil
+  end
+  
+  def self.resolve_direction(heading, side)
+    heading = DIRECTIONS[heading]
+    side = DIRECTIONS[side]
+    
+    offset = (side - heading) % 360
+    
+    # offset is the left/right direction is preserved, but heading adjusted to be North
+    return nil if offset % 180 == 0 # 0/360, 180 are direct forward/backward not left right
+    
+    offset > 180 ? :left : :right
+  end
   #STUBS  
   def left
     {"flag" => 'meter', "message" => 'until 8pm'}
--- test/unit/parking_test.rb
+++ test/unit/parking_test.rb
@@ -17,4 +17,61 @@ class ParkingTest < Test::Unit::TestCase
                 "right" => {"flag" => 'ok'} }
     assert_equal expected, Parking.new(:address => ADDRESS, :heading => HEADING).as_json
   end
+  
+  def test_resolve_direction
+    # heading north, and west is a side, west is on the left
+    assert_equal :left, Parking.resolve_direction('N', 'W')
+    assert_equal :right, Parking.resolve_direction('N', 'E')
+    
+    assert_equal :right, Parking.resolve_direction('S', 'W')
+    assert_equal :left, Parking.resolve_direction('S', 'E')
+    
+    assert_equal :left, Parking.resolve_direction('E', 'N')
+    assert_equal :right, Parking.resolve_direction('W', 'N')
+    
+    assert_equal :left, Parking.resolve_direction('W', 'S')
+    assert_equal :right, Parking.resolve_direction('E', 'S')
+    
+    assert_equal nil, Parking.resolve_direction('W', 'W')
+    assert_equal nil, Parking.resolve_direction('W', 'E')
+  end
+  
+  def test_normalize_distance
+    assert_equal 900, Parking.normalize_distance("900 feet")
+    assert_equal 1056, Parking.normalize_distance("0.2 miles")
+    assert_equal nil, Parking.normalize_distance('ajkshasjkhd')
+  end
+  
+  def test_parking_computes_correctly_for_valid_left
+    #seg = segment('W', false)
+    #assert_equal [15840, :left, seg], Parking.primo_message(seg, 'N')
+
+    #seg = segment('E', false)
+    #assert_equal [15840, :right, seg], Parking.primo_message(seg, 'N')
+    
+  end
+  
+  def test_parking_parses_correctly_for_invalid_left
+    #flunk
+  end
+  
+  def test_parking_computes_correctly_for_valid_left_and_right
+    #flunk
+  end
+  
+  def test_parking_computes_correctly_for_multiple_matches
+    #flunk
+  end
+  
+  private
+  
+  def segment(side, meter, distance="3 miles", section = "Alewife - Newbury", limit='4 days')
+  { :limit=>limit,
+    :meter=>meter,
+    :side=>side,
+    :distance=>distance,
+    :until=>"Thu&nbsp;12:00am",
+    :street=>"Huntington Ave",
+    :section=>"Mass Ave-Harcourt St"}  
+  end
 end
