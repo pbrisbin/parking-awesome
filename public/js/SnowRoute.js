7691cf4a3ea410f3499cbfbd118cd7cd07d6aa5b
patrick brisbin <pbrisbin@gmail.com>
2011-02-26 13:29:38 -0500

    isSnowRoute takes position now

--- public/js/SnowRoute.js
+++ public/js/SnowRoute.js
@@ -1,7 +1,10 @@
-/* look for snow route features at the given point; if features, call 
+/* look for snow route features at the given position; if features, call 
  * success(features), else call failure (optional).
  */
-function isSnowRoute(lng, lat, success, failure) {
+function isSnowRoute(position, success, failure) {
+  var lng = position.coord.longitude;
+  var lat = position.coord.latitude;
+
   civicAPI('bos_snow_routes', lng, lat, function(data) {
     if (data.features.length) {
       success(data.features);
