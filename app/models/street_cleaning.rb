13768068e4e51e19afd5911c4fc4db2bfaf7734c
Ken Mazaika <kmazaika@gmail.com>
2011-02-26 21:17:35 -0500

    add validations to street cleaning consumer

--- app/models/street_cleaning.rb
+++ app/models/street_cleaning.rb
@@ -1,4 +1,13 @@
 class StreetCleaning
+  attr_accessor :side_of_evens, :street_name
+  include ActiveModel::Validations
+  validates :side_of_evens, :presence => true
+  validates :street_name, :presence => true
   
+  def initialize(attributes = {})
+    attributes.each do |attribute, value|
+      self.send("#{attribute}=".to_sym, value)
+    end
+  end
   
 end
--- test/unit/street_cleaning_test.rb
+++ test/unit/street_cleaning_test.rb
@@ -3,8 +3,16 @@ require 'fakeweb'
 
 class StreetCleaningTest < Test::Unit::TestCase
 
-  def test_flunk
-    flunk
+  def test_initialize
+    sc = StreetCleaning.new(:side_of_evens => 'left', :street_name => 'newbury')
+    assert_equal true, sc.valid?
+  end
+  
+  def test_initialize_invalid_constructor
+    sc = StreetCleaning.new
+    assert_equal false, sc.valid?
+    assert_equal "can't be blank", sc.errors[:side_of_evens].first
+    assert_equal "can't be blank", sc.errors[:street_name].first
   end
 
 end
