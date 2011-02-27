324d76e7a0339a31f4cd3cd23ae2fac18d2a3e91
Ken Mazaika <kmazaika@gmail.com>
2011-02-26 19:11:29 -0500

    add test for only passing lat lon to api

--- test/functional/parking_spots_controller_test.rb
+++ test/functional/parking_spots_controller_test.rb
@@ -14,4 +14,16 @@ class ParkingSpotsControllerTest < ActionController::TestCase
     
     assert_equal expected, ActiveSupport::JSON.decode(@response.body)
   end
+  
+  
+  def test_index_success_with_lat_lon
+    get :index, :latitude => 42.3478, :longitude => -71.080637, :heading => 'N',:format => :json
+    assert_response :success
+
+    expected = {"left"=>{'flag'=>"no", 'message'=>"bad idea"},
+     "right"=>{'flag'=>"no", 'message'=>"bad idea"}, "streetname"=>"Huntington Ave"}
+    
+    assert_equal expected, ActiveSupport::JSON.decode(@response.body)
+  end
+  
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,6 +10,8 @@ def setup_primo_fakeweb
     :body => File.open('test/fixtures/primo_huntington_page2.html').read
   FakeWeb.register_uri :get, "http://primospot.com/m/results?address=Huntington+Ave%2C+Boston&commit=Search&page=3",
     :body => File.open('test/fixtures/primo_huntington_page3.html').read
+  FakeWeb.register_uri :get, "http://primospot.com/m/results?address=Huntington%20Ave&commit=Search",
+    :body => File.open('test/fixtures/primo_huntington.html').read
     
   FakeWeb.register_uri :get, "http://primospot.com/m/results?address=302%20Newbury%20St&commit=Search",
       :body => File.open('test/fixtures/primo_huntington.html').read
@@ -22,6 +24,10 @@ def setup_geocoder_fakeweb
     
   FakeWeb.register_uri :get, "http://maps.google.com/maps/api/geocode/json?sensor=false&address=Huntington+Ave%2C+Boston",
     :body => File.open('test/fixtures/huntington_ave_boston.json').read
+    
+    FakeWeb.register_uri :get, "http://maps.google.com/maps/api/geocode/json?latlng=42.3478%2C-71.080637&sensor=false",
+      :body => File.open('test/fixtures/huntington_ave_boston.json').read
+    
 end
 
 class ActiveSupport::TestCase
