128e5dfc6a10f25fd1875ce5b2c8e4e73d8e73e7
patrick brisbin <pbrisbin@gmail.com>
2011-02-26 20:48:44 -0500

    cleanup, remove extra comments

--- public/js/Vehicle.js
+++ public/js/Vehicle.js
@@ -1,144 +1,151 @@
-/* A better heading representation for our purposes */
-Direction = {
-  NULL      : 0, // no heading info
-  NORTH     : 1,
-  NORTHEAST : 2,
-  EAST      : 3,
-  SOUTHEAST : 4,
-  SOUTH     : 5,
-  SOUTHWEST : 6,
-  WEST      : 7,
-  NORTHWEST : 8
-}
+/* this "API" is documented at https://gist.github.com/845751 */
 
-SideOfVehicle = {
-  Left:  0,
-  Right: 1
-}
-
-/* usage:
- *
- * var h = new Heading(heading);
- * print(h.toString());
- *
- */
-function Heading(heading) {
-  this.heading   = heading;
-  this.direction = directionFromBearing(this.heading);
-}
-
-/* display a heading friendly style */
-Heading.prototype.toString = function() {
-  switch(this.direction) {
-    case Direction.NULL     : return "null";
-    case Direction.NORTH    : return "N";
-    case Direction.NORTHEAST: return "NE";
-    case Direction.EAST     : return "E";
-    case Direction.SOUTHEAST: return "SE";
-    case Direction.SOUTH    : return "S";
-    case Direction.SOUTHWEST: return "SW";
-    case Direction.WEST     : return "W";
-    case Direction.NORTHWEST: return "NW";
-  }
-}
-
-/* usage:
- *
- * var v = new Vehicle(position);
- * print(v.toString());
- *
- */
-function Vehicle(position) { // {{{
-  this.position  = position;
-  this.longitude = position.coords.longitude;
-  this.latitude  = position.coords.latitude;
-  this.heading   = new Heading(position.coords.heading);
+Direction = { // {{{
+    NULL      : 0, // no heading info
+    NORTH     : 1,
+    NORTHEAST : 2,
+    EAST      : 3,
+    SOUTHEAST : 4,
+    SOUTH     : 5,
+    SOUTHWEST : 6,
+    WEST      : 7,
+    NORTHWEST : 8
 } // }}}
 
-/* use the browser's geolocation info to construct the vehicle object
- * and call the success callback on it
- */
+SideOfVehicle = { // {{{
+    Left:  0,
+    Right: 1
+} // }}}
+
+function oppositeSide(side) { // {{{
+    switch (side) {
+        case SideOfVehicle.Left:  return SideOfVehicle.Right;
+        case SideOfVehicle.Right: return SideOfVehicle.Left;
+    }
+} // }}}
+
+function Heading(heading) { // {{{
+    this.heading   = heading;
+    this.direction = directionFromBearing(this.heading);
+} // }}}
+
+Heading.prototype.toString = function() { // {{{
+    switch(this.direction) {
+        case Direction.NULL     : return "null";
+        case Direction.NORTH    : return "N";
+        case Direction.NORTHEAST: return "NE";
+        case Direction.EAST     : return "E";
+        case Direction.SOUTHEAST: return "SE";
+        case Direction.SOUTH    : return "S";
+        case Direction.SOUTHWEST: return "SW";
+        case Direction.WEST     : return "W";
+        case Direction.NORTHWEST: return "NW";
+    }
+} // }}}
+
+function Vehicle(position) { // {{{
+    this.position  = position;
+    this.longitude = position.coords.longitude;
+    this.latitude  = position.coords.latitude;
+    this.heading   = new Heading(position.coords.heading);
+} // }}}
+
 function initVehicleFromGeoLocation(success, error, unsupported) { // {{{
-  var v = null;
+    if (!navigator.geolocation) {
+        unsupported;
+        return;
+    }
 
-  if (!navigator.geolocation) {
-    unsupported;
-    return;
-  }
-
-  navigator.geolocation.getCurrentPosition(function(position) {
-    v = new Vehicle(position);
-    success(v);
-  }, error);
+    navigator.geolocation.getCurrentPosition(function(position) {
+        success(new Vehicle(position));
+    }, error);
 } // }}}
 
 Vehicle.prototype.toString = function() { // {{{
-  return "vehicle is at (" 
-    + this.longitude + ", " 
-    + this.latitude + ") heading " 
-    + this.heading.toString();
+    return "{ vehicle: { " 
+        + this.longitude + ", " 
+        + this.latitude + " }, " 
+        + this.heading.toString() + " }";
 } // }}}
 
-/* given a position, report if it's determined to be on the Left of 
- * Right side of the Vehicle Object
- */
 Vehicle.prototype.sideOfVehicle = function(position) { // {{{
-  var adjustedDirection = null; 
-  var diffLong = this.longitude - position.coords.longitude;
-  var diffLat  = this.latitude  - position.coords.latitude;
+    var adjustedDirection = null; 
+    var diffLong = this.longitude - position.coords.longitude;
+    var diffLat  = this.latitude  - position.coords.latitude;
 
-  var northSouth = diffLong >= 0 ? Direction.NORTH : Direction.SOUTH;
-  var eastWest   = diffLat  >= 0 ? Direction.EAST  : Direction.WEST;
+    var northSouth = diffLong >= 0 ? Direction.NORTH : Direction.SOUTH;
+    var eastWest   = diffLat  >= 0 ? Direction.EAST  : Direction.WEST;
 
-  /* this might not make sense... */
-  var strongerEastWest = Math.abs(diffLat) >= Math.abs(diffLong) ? true : false;
+    /* this might not make sense... */
+    var strongerEastWest = Math.abs(diffLat) >= Math.abs(diffLong) ? true : false;
 
-  switch(this.heading.direction) {
-    /* fall back is just a guess */
-    case Direction.NULL:  return SideOfVehicle.Right;
+    switch(this.heading.direction) {
+        /* fall back is just a guess */
+        case Direction.NULL:  return SideOfVehicle.Right;
 
-    /* leave these as-is */
-    case Direction.NORTH:
-    case Direction.SOUTH:
-    case Direction.EAST:
-    case Direction.WEST:
-      adjustedDirection = this.heading.direction;
+                              /* leave these as-is */
+        case Direction.NORTH:
+        case Direction.SOUTH:
+        case Direction.EAST:
+        case Direction.WEST:
+                              adjustedDirection = this.heading.direction;
 
-    /* reset from NW to N or W depending on the value of 
-     * strongerEastWest */
-    case Direction.NORTHEAST: 
-      direction = strongerEastWest ? Direction.NORTH : Direction.EAST;
-      break;
+                              /* reset from NW to N or W depending on the value of 
+                               * strongerEastWest */
+        case Direction.NORTHEAST: 
+                              direction = strongerEastWest ? Direction.NORTH : Direction.EAST;
+                              break;
 
-    case Direction.SOUTHEAST: 
-      direction = strongerEastWest ? Direction.SOUTH : Direction.EAST;
-      break;
+        case Direction.SOUTHEAST: 
+                              direction = strongerEastWest ? Direction.SOUTH : Direction.EAST;
+                              break;
 
-    case Direction.SOUTHWEST: 
-      direction = strongerEastWest ? Direction.SOUTH : Direction.WEST;
-      break;
+        case Direction.SOUTHWEST: 
+                              direction = strongerEastWest ? Direction.SOUTH : Direction.WEST;
+                              break;
 
-    case Direction.NORTHWEST: 
-      direction = strongerEastWest ? Direction.NORTH : Direction.WEST;
-      break;
-  }
+        case Direction.NORTHWEST: 
+                              direction = strongerEastWest ? Direction.NORTH : Direction.WEST;
+                              break;
+    }
 
-  switch(adjustedDirection) {
-    case Direction.NORTH: return eastWest   == Direction.WEST  ? SideOfVehicle.Left : SideOfVehicle.Right;
-    case Direction.SOUTH: return eastWest   == Direction.EAST  ? SideOfVehicle.Left : SideOfVehicle.Right;
-    case Direction.EAST:  return northSouth == Direction.SOUTH ? SideOfVehicle.Left : SideOfVehicle.Right;
-    case Direction.WEST:  return northSouth == Direction.SOUTH ? SideOfVehicle.Left : SideOfVehicle.Right;
-  }
+    switch(adjustedDirection) {
+        case Direction.NORTH: return eastWest   == Direction.WEST  ? SideOfVehicle.Left : SideOfVehicle.Right;
+        case Direction.SOUTH: return eastWest   == Direction.EAST  ? SideOfVehicle.Left : SideOfVehicle.Right;
+        case Direction.EAST:  return northSouth == Direction.SOUTH ? SideOfVehicle.Left : SideOfVehicle.Right;
+        case Direction.WEST:  return northSouth == Direction.SOUTH ? SideOfVehicle.Left : SideOfVehicle.Right;
+    }
 } // }}}
 
-/* location the address nearest to the vehicle */
 Vehicle.prototype.sideOfEvens = function(callback) { // {{{
-  var feature;
+    var vehicle = this;
+    var feature;
+    var addrPos;
+    var addrNum;
+    var addrSide;
 
-  civicApiBox("bos_addresses", this.position, 0.01, function(data) {
-    feature = nearestFeature(this.position, data.features);
-    //callback(feature.properties.properties.full_addre);
-    // todo:
-    callback(SideOfVehicle.Right);
-  });
+    function parseNum(addr) {
+        var word = addr.substring(0, addr.indexOf(' '));
+        var nums = word.replace(/[^0-9]/g, '');
+        return nums.substring(nums.length - 1, nums.length);
+    }
+
+    civicApiBox("bos_addresses", this.position, 0.005, function(data) {
+        feature = nearestFeature(vehicle.position, data.features);
+
+        addrNum = parseNum(feature.properties.properties.full_addre);
+        addrPos = {
+            coords: {
+                longitude: feature.geometry.coordinates[0],
+                latitude:  feature.geometry.coordinates[1]
+            }
+        }
+
+        addrSide = vehicle.sideOfVehicle(addrPos);
+        
+        if (Number(addrNum) % 2 == 0)
+            callback(addrSide);
+        else
+            callback(oppositeSide(addrSide));
+    });
 } // }}}
