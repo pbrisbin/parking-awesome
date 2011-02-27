1bdd2c428cf3e4b0b66552b03f138f09c813a73c
Ken Mazaika <kmazaika@gmail.com>
2011-02-26 20:38:45 -0500

    update boston scraper gem

---
+++ app/models/street_cleaning.rb
@@ -0,0 +1,4 @@
+class StreetCleaning
+  
+  
+end
---
+++ test/unit/street_cleaning_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+require 'fakeweb'
+
+class StreetCleaningTest < Test::Unit::TestCase
+
+  def test_flunk
+    #flunk
+  end
+
+end
