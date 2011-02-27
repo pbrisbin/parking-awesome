519b1cd381454b7a81056f0b714bab704b527321
patrick brisbin <pbrisbin@gmail.com>
2011-02-26 22:27:57 -0500

    whitespace fixes

--- app/controllers/snow_emergency_controller.rb
+++ app/controllers/snow_emergency_controller.rb
@@ -1,25 +1,23 @@
 class SnowEmergencyController < ApplicationController
+  # assumption: if our mailbox has a mail with "Snow emergency" in the 
+  # subject, which was sent in the past 24 hours, then a snow emergency is 
+  # in effect.
+  def snow_emergency_in_effect
+    require 'net/imap'
 
-require 'net/imap'
+    ret = false
 
-# assumption: if our mailbox has a mail with "Snow emergency" in the 
-# subject, which was sent in the past 24 hours, then a snow emergency is 
-# in effect.
-def snow_emergency_in_effect
-  ret = false
+    imap = Net::IMAP.new('imap.gmail.com',  { :port => 993, :ssl => true })
+    imap.login('canhazparking', 'yesIcanz!');
+    imap.select('INBOX')
+    imap.search(["SUBJECT", "Snow emergency", "SENTSINCE", (Time.now - 1).strftime("%d-%b-%Y")]).each do |msgid|
+      ret = true
+      break
+    end
 
-  imap = Net::IMAP.new('imap.gmail.com',  { :port => 993, :ssl => true })
-  imap.login('canhazparking', 'yesIcanz!');
-  imap.select('INBOX')
-  imap.search(["SUBJECT", "Snow emergency", "SENTSINCE", (Time.now - 1).strftime("%d-%b-%Y")]).each do |msgid|
-    ret = true
-    break
+    imap.logout()
+    imap.disconnect()
+
+    return ret
   end
-
-  imap.logout()
-  imap.disconnect()
-
-  return ret
-end
-
 end
